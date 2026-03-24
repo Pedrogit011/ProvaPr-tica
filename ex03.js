be7049f65c05bb16f8b5ed661ec32dfc29cd54e1
Pedrogit011 <pedro.silva01102625@sesisenaipr.org.br>
2026-03-24 10:51:35 -0300

    prova

---
+++ ex03.js
@@ -0,0 +1,6 @@
+let idade =16
+if(idade =18){
+    console.log(ele é maior de idade)
+    }else if
+    console.log(ele é menor de idade)
+}
---
+++ ex04.js
@@ -0,0 +1,10 @@
+let nota=7
+if(nota>=7){
+    console.log('voce foi aprovado')
+}else if
+    if(nota)>=5{
+        console.log('você está de recuperação')
+    }else if
+if(nota<=5){
+        console.log('você foi reprovado')
+    }
